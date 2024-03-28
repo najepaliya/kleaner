fce673aff18a8e3a07b843f0cbb1e56cac15b961
Neel Jepaliya <najepaliya@gmail.com>
2024-03-27 21:19:37 -0700

    implement template view

---
+++ frontend/home/TemplateView.qml
@@ -0,0 +1,8 @@
+import "TemplateViewUtilities.js" as U
+
+AbstractView {
+    viewTitle: "Templates"
+    viewModel: 4
+    headerButtonList: U.headerButtonList
+    rowButtonList: U.rowButtonList
+}
---
+++ frontend/home/TemplateViewUtilities.js
@@ -0,0 +1,23 @@
+const headerButtonList = [
+    {
+        icon: "list-add",
+        function: () => {
+            console.log("list added")
+        }
+    }
+]
+
+const rowButtonList = [
+    {
+        icon: "edit-entry",
+        function: () => {
+            console.log("row edited")
+        }
+    },
+    {
+        icon: "edit-delete",
+        function: () => {
+            console.log("row deleted")
+        }
+    }
+]
